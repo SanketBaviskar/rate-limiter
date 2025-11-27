85fde26014fe4e648407e14ef7fb455bd6a80f0a
Sanket Baviskar <sanketbaviskar01@gmail.com>
2025-11-27 13:59:49 -0700

    url update

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ app = FastAPI(title="Rate Limiter Project")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
